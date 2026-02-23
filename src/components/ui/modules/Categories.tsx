dfd2e8b3fe96dc23c81b39f74c7b128d05a7d39f
Rohan Shariar <rhnshariar@gmail.com>
2026-02-23 20:41:38 +0600

    gitignore edited

--- src/components/ui/modules/Categories.tsx
+++ src/components/ui/modules/Categories.tsx
@@ -1,32 +1,32 @@
-const img1 = "/assets/image/product1.jpg"
+const img1 = "/assets/image/product1.jpg";
 
 const Categories = () => {
 	return (
 		<div className=" bg-black py-7 px-6">
 			<div className="flex justify-between">
-                <h1 className="uppercase text-7xl font-bold text-white">Categories</h1>
-			<div className="join flex">
-				<button className="join-item btn mx-2 bg-white text-black border-0">
-					«
-				</button>
-				<button className="join-item btn mx-2 bg-white text-black border-0">
-					»
-				</button>
+				<h1 className="uppercase text-7xl font-bold text-white">Categories</h1>
+				<div className="join flex">
+					<button className="join-item btn mx-2 bg-white text-black border-0">
+						«
+					</button>
+					<button className="join-item btn mx-2 bg-white text-black border-0">
+						»
+					</button>
+				</div>
 			</div>
-            </div>
-            <div className="card relative w-96 overflow-hidden bg-base-100 shadow-sm">
-              <figure className="h-full w-full">
-                <img src={img1} alt="Shoes" className="h-full w-full object-cover" />
-              </figure>
-              <div className="card-body absolute bottom-0 left-0 right-0 text-white">
-                <div className="flex justify-between">
-                    <h2 className="card-title uppercase text-2xl">Card Title</h2>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Buy Now</button>
-                </div>
-                </div>
-              </div>
-            </div>
+			<div className="card relative w-96 overflow-hidden bg-base-100 shadow-sm">
+				<figure className="h-full w-full">
+					<img src={img1} alt="Shoes" className="h-full w-full object-cover" />
+				</figure>
+				<div className="card-body absolute bottom-0 left-0 right-0 text-white">
+					<div className="flex justify-between">
+						<h2 className="card-title uppercase text-2xl">Card Title</h2>
+						<div className="card-actions justify-end">
+							<button className="btn btn-primary">Buy Now</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
