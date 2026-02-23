23378bd2bcf4b14840cedc16d16dc8d87e62bef1
Rohan Shariar <rhnshariar@gmail.com>
2026-02-23 23:54:06 +0600

    error solved

--- src/components/ui/shared/Header.tsx
+++ src/components/ui/shared/Header.tsx
@@ -20,40 +20,61 @@ const Header = () => {
     </>
 
     return (
-        <div className='flex justify-center'>
-            <div className="navbar bg-white shadow-sm w-[1320px]">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        {items}
-      </ul>
-    </div>
-    <ul className="hidden lg:flex menu menu-horizontal px-1">
-     {items}
-    </ul>
-  </div>
-  <div className="navbar-center">
-    <a className=""><img src={logo} alt="" /></a>
-  </div>
-  <div className="navbar-end gap-4">
-    <button className="btn btn-ghost btn-circle">
-      <Search size={20} />
-    </button>
-    <button className="btn btn-ghost btn-circle">
-      <User size={20} />
-    </button>
-    <button className="btn btn-ghost btn-circle">
-      <ShoppingCart size={20} />
-    </button>
-  </div>
-</div>
-        </div>
-    );
+			<div className="flex justify-center">
+				<div className="navbar bg-white shadow-sm w-[1320px]">
+					<div className="navbar-start">
+						<div className="dropdown">
+							<div
+								tabIndex={0}
+								role="button"
+								className="btn btn-ghost lg:hidden"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-5 w-5"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									{" "}
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M4 6h16M4 12h8m-8 6h16"
+									/>{" "}
+								</svg>
+							</div>
+							<ul
+								tabIndex={-1}
+								className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+							>
+								{items}
+							</ul>
+						</div>
+						<ul className="hidden lg:flex menu menu-horizontal px-1">
+							{items}
+						</ul>
+					</div>
+					<div className="navbar-center">
+						<a className="">
+							<img src={logo} alt="" />
+						</a>
+					</div>
+					<div className="navbar-end gap-4">
+						<button className="btn btn-ghost btn-circle">
+							<Search size={20} />
+						</button>
+						<button className="btn btn-ghost btn-circle">
+							<User size={20} />
+						</button>
+						<button className="btn btn-ghost btn-circle">
+							<ShoppingCart size={20} />
+						</button>
+					</div>
+				</div>
+			</div>
+		);
 };
 
 export default Header;
